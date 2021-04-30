9fa90519c8625c841108880b03121023d680035d
HelenaIsh <elena-ismatova@mail.ru>
2021-04-30 22:18:01 +0500

    fix front

--- src/ClientApp/src/components/Field/index.jsx
+++ src/ClientApp/src/components/Field/index.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import styles from "./styles.module.css";
 import Person from "../Person";
 import House from "../House";
-import {v4 as uuidv4} from 'uuid';
 
 export default function Field({ map, people, onClick }) {
   return (
     <div className={styles.root}>
       {map.map((item, i) => (
-        <House key={uuidv4()} x={item.x} y={item.y} />
+        <House key={item.id} x={item.x} y={item.y} />
       ))}
       {people.map((item) => (
         <Person person={item} key={item.id} onClick={onClick} />
