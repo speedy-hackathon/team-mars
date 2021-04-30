4de40418a581b166c79d1d6f86b5c83c659ecec4
HelenaIsh <elena-ismatova@mail.ru>
2021-04-30 21:00:12 +0500

    add red color for sick

--- src/ClientApp/src/components/Person/index.jsx
+++ src/ClientApp/src/components/Person/index.jsx
@@ -5,9 +5,11 @@ import { MAX_HEIGHT, MAX_WIDTH } from "../../consts/sizes";
 export default function Person({ person, onClick }) {
   const x = (person.position.x / MAX_WIDTH) * 100;
   const y = (person.position.y / MAX_HEIGHT) * 100;
+  const isSick = person.isSick;
+
   return (
     <div
-      className={styles.root}
+        className={styles.root + (isSick? " " + styles.sick : "")}
       style={{ left: `${x}%`, top: `${y}%` }}
       onClick={() => onClick(person.id)}
     />
