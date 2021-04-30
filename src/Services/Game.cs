58f64da86a7aa89f92d22c621eba25d85874735b
Алишер Казанцев <alisher021299@gmail.com>
2021-04-30 23:03:29 +0500

    Add ticks field to Game class

--- src/Services/Game.cs
+++ src/Services/Game.cs
@@ -8,6 +8,7 @@ namespace covidSim.Services
     {
         public List<Person> People;
         public CityMap Map;
+        public int Ticks = 0;
         private DateTime _lastUpdate;
 
         private static Game _gameInstance;
@@ -79,6 +80,7 @@ namespace covidSim.Services
             {
                 person.CalcNextStep();
             }
+            Ticks += 1;
         }
     }
 }
